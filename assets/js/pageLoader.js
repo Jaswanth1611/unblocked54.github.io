e2b3fe36c18e8e8ca316150e69a58701e91a086f
MousyMichael <91567101+MousyMichael@users.noreply.github.com>
2024-02-24 11:16:07 -0500

    Update pageLoader.js

--- assets/js/pageLoader.js
+++ assets/js/pageLoader.js
@@ -1,13 +1,70 @@
-<div class="sideBarHolder"><div id="sideBar" class="sideBar home"><a onclick=""><span style="display:unset!important" class="material-symbols-outlined">apps</span><span>&nbsp;Home</span></a><a><span style="display:unset!important" class="material-symbols-outlined">search</span><span>&nbsp;Search</span></a><a><span style="display:unset!important" class="material-symbols-outlined">settings</span><span>&nbsp;Settings</span></a><a><span style="display:unset!important" class="material-symbols-outlined">close</span><span>&nbsp;Close Tab</span></a></div></div><div class="contentContainer"><div id="pageContent" class="content home"><div class="contentHolder"><div class="content-news"><div class="carousel"></div></div><div class="content-games"><div class="game-row"><div class="category-name">Popular Games</div><div class="game-tile-holder"><div class="game-tile"><div class="game-image"><img src="https://cdn.akamai.steamstatic.com/steam/apps/2305790/capsule_616x353.jpg?t=1694685389"><div class="game-name">1v1.LOL</div></div></div><div class="game-tile"><div class="game-image"><img src="https://th.bing.com/th/id/OIP.9uEq0e1ME5XyhKPi-yZK-gHaEK?rs=1&pid=ImgDetMain"><div class="game-name">Shell Shockers</div></div></div><div class="game-tile"><div class="game-image"><img src="https://wallpaperaccess.com/full/2184838.jpg"><div class="game-name">Roblox</div></div></div><div class="game-tile"><div class="game-image"><img src="https://th.bing.com/th/id/OIP.b78tTmd8mOKKVr1CuLs9QAHaEK?rs=1&pid=ImgDetMain"><div class="game-name">BitLife</div></div></div><div class="game-tile"><div class="game-image"><img src="https://th.bing.com/th/id/OIP.HGRRtFdLoE2taBuSSluS7AAAAA?rs=1&pid=ImgDetMain"><div class="game-name">Tunnel Rush</div></div></div><div class="game-tile"><div class="game-image"><img src="https://cdn.cloudflare.steamstatic.com/steam/apps/1454400/header.jpg?t=1630523196"><div class="game-name">Cookie Clicker</div></div></div></div></div><div class="content-footer"></div></div></div></div><script src="assets/js/application.js"></script><script src="assets/js/simple-search-v1.js"></script>
+/*
+-> A "page loader" for Unblocked54 GitHub
+-> You can customize the config below to your liking
+-> Developed By MousyMichael
+*/
 
 var pLoaderConfig = {
-  
+  defaultLocation: "",
+  settingsKey: "",
 };
 
-window.onload = startLoad(); 
+window.onload = loadManager();
 
-funciton startLoad() {
- if(window.self !== window.top) {
-   document.getElementBy
- }
+function loadManager() {
+   var framedStatus = checkTop();
+   var simpleSearchStatus = startSimpleSearch();
+   var settingsStatus = loadSettings();
+  	
+   if(framedStatus == normal) {
+   	if(settingsStatus == loaded) {
+      if(settings.hideWindowOnLoad == true) {
+        hideWindow();
+      } else {
+      	if(simpleSearchStatus == loaded) {
+          
+        } else {
+         } 
+      } 
+    } else {
+      if(settingsStatus == mal | settingsStatus == null | settingsStatus < 0) {
+        loadingError("This page appears to be being loaded by an unauthorized source!")
+      } else {
+        loadingError("Unable to determine what went wrong. Most likley a return error! Try again later.")
+       }
+     }  
+   } else {
+   if(framedStatus == hidden) 
+  } 
+}
+
+function checkTop() {
+if(window.self !== window.top) {
+	return mal
+	} else {
+   	if(window.href !== pLoaderConfig.defaultLocation) {
+    	if(window.hostname == "about:blank") {
+        	return hidden
+        	} else {
+              return mal
+           } 
+    	} else {
+          return normal
+       }
+    }
+} 
+
+function loadSettings() {
+	if(checkTop() == hidden) {
+    	var settings = window.settingsHandOff;
+      	console.log("Settings have been loaded from the handoff varible")
+      	return loaded
+   } else {
+   	if(checkTop() == normal) {
+      var settings = JSON.parse(localStorage.getItems(pLoaderConfig.settingsKey));
+      return loaded
+    } else {
+      return mal 
+     } 
+   }
 }
